0f41cc7c0c71be89ab17e6baed08b43f04c3c7d8
Setricmuholo <muholosetric@gmail.com>
2023-07-02 22:40:11 +0300

    fixed pycodestyle errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,8 @@
 """
 a function that print the pascal triangle
 """
+
+
 def pascal_triangle(n):
     if n <= 0:
         return []
@@ -16,4 +18,3 @@ def pascal_triangle(n):
         triangle.append(current_row)
 
     return triangle
-
