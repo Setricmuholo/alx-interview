040e13be8454287e2f07df8db2caf61ea551edf9
Setricmuholo <muholosetric@gmail.com>
2023-07-13 20:12:07 +0300

    created a minimum function

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def minOperations(n):
+    """
+    minimum operation function
+    """
+    if n == 1:
+        return 0  # No operations needed for a single 'H'
+
+    operations = 0
+    factor = 2  # Represents the number of 'H's currently in the file
+
+    while factor <= n:
+        if n % factor == 0:
+            n //= factor
+            operations += factor
+        else:
+            factor += 1
+
+    return operations
+
+
