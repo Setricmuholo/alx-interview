0c9ffda6e115a9e4fe196b7a995c4fb7b651d631
Setricmuholo <muholosetric@gmail.com>
2023-08-10 22:59:56 +0300

    completed task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,47 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-function getMovieCharacters(movieId) {
-  const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-  request(filmUrl, (error, response, filmBody) => {
-    if (error) {
-      console.error(`Error fetching movie data: ${error}`);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error(`Error: Unable to retrieve data for movie ID ${movieId}`);
-      return;
-    }
-
-    const filmData = JSON.parse(filmBody);
-    const characters = filmData.characters;
-
-    console.log(`Characters from ${filmData.title} (Episode ${filmData.episode_id}):`);
-    characters.forEach(characterUrl => {
-      request(characterUrl, (error, response, characterBody) => {
-        if (error) {
-          console.error(`Error fetching character data: ${error}`);
-          return;
-        }
-
-        if (response.statusCode === 200) {
-          const characterData = JSON.parse(characterBody);
-          console.log(characterData.name);
-        } else {
-          console.error(`Error: Unable to retrieve character data from ${characterUrl}`);
-        }
-      });
-    });
-  });
-}
-
-if (process.argv.length !== 3) {
-  console.log('Usage: node star_wars_characters_request.js <movie_id>');
-  process.exit(1);
-}
-
 const movieId = process.argv[2];
-getMovieCharacters(movieId);
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
+
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+};
